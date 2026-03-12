ef6b777d665a4d3dd54b05f99ee33ee0a1258118
michael-hmrc <47248104+michael-hmrc@users.noreply.github.com>
2026-03-12 17:44:51 +0000

    wipg

--- src/cantonese_tts/config.py
+++ src/cantonese_tts/config.py
@@ -2,4 +2,6 @@ import os
 
 MODEL = os.getenv("OPENAI_TTS_MODEL", "gpt-4o-mini-tts")
 VOICE = os.getenv("OPENAI_TTS_VOICE", "alloy")
+# VOICE = os.getenv("OPENAI_TTS_VOICE", "marin")
+# VOICE = os.getenv("OPENAI_TTS_VOICE", "cedar")
 FORMAT = "mp3"
--- src/cantonese_tts/generator.py
+++ src/cantonese_tts/generator.py
@@ -4,29 +4,21 @@ from .config import MODEL, VOICE, FORMAT
 
 
 def generate_audio(text: str, output_path: Path):
-    
     client = OpenAI()
-    
+
     output_path.parent.mkdir(parents=True, exist_ok=True)
     print(f"🔊 Generating audio for: {text}")
-    
-    
-    # prompt = f"用廣東話讀: \n{text}"
-    
-    prompt = f"""
-Read the following text in natural Hong Kong Cantonese.
-Use native Cantonese pronunciation.
-Do not translate.
-Do not add extra words.
-Only speak the exact text provided.
-
-{text}
-"""
 
     with client.audio.speech.with_streaming_response.create(
         model=MODEL,
         voice=VOICE,
-        input=prompt,
+        input=text,
+        instructions=(
+            "Speak in natural Hong Kong Cantonese. "
+            "Use native Cantonese pronunciation. "
+            "Do not translate. "
+            "Do not add extra words."
+        ),
         response_format=FORMAT
     ) as response:
         response.stream_to_file(output_path)
